0731bc1d9aca1f9d57bdc21a01b0d18864d5c999
Allen Jiang <yanpingjiang001@gmail.com>
2016-01-07 00:52:49 -0500

    update

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,30 @@
+class SearchController < ApplicationController
+  def index
+  end
+
+  def create
+  
+    keywords = params[:keywords]
+    phrase = params[:phrase]
+    @response = Array.new()
+    
+    if keywords == 'people'
+      users = User.where("name like ?","%#{phrase}%")
+      users.each do |user|
+        @response.push(user)
+      end
+    else if keywords == 'zim'
+      zims = Zim.where("name like ?","%#{phrase}%")
+      users.each do |user|
+        @response.push(user)
+      end
+    end
+    
+    respond_to do |format|
+      format.html {render json: @response,status: :ok}
+      format.json {render json: @response,status: :ok}
+    end    
+  end
+  
+  
+end
